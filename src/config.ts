e1ee77c2d5470c40161e24ec72796e9bd4e86b33
Sallanez <saul.alejandro19@gmail.com>
2023-09-26 15:52:30 -0600

    RAILWAY_PUBLIC_DOMAIN .env added to port

--- src/config.ts
+++ src/config.ts
@@ -10,8 +10,8 @@ export const MONGO_USER =
 export const MONGO_PASSWORD =
   envFound.parsed?.API_MONGO_PASSWORD || process.env.API_MONGO_PASSWORD;
 export const ORIGIN_DOMAIN = `${
-  envFound.parsed?.API_ORIGIN_DOMAIN || process.env.API_ORIGIN_DOMAIN
-}:${envFound.parsed?.API_ORIGIN_PORT || process.env.API_ORIGIN_PORT}`;
+  envFound.parsed?.API_ORIGIN_DOMAIN || process.env.RAILWAY_PUBLIC_DOMAIN
+}:${envFound.parsed?.API_ORIGIN_PORT || process.env.API_PORT}`;
 
 //For the script set-env.ts
 export const NODE_API_PORT = envFound.parsed?.API_PORT || process.env.API_PORT;
