b1525bbe22bdb8130a5c3c46458a45e9767e667b
Sallanez <saul.alejandro19@gmail.com>
2023-09-26 12:34:34 -0600

    some minor erro with the types in the api on auht/verifyController

--- src/controllers/Auth/verifyController.ts
+++ src/controllers/Auth/verifyController.ts
@@ -9,19 +9,23 @@ export const verifyToken = async (req: Request, res: Response) => {
   const { token }: { token: string } = req.cookies;
   if (!token)
     return res.status(401).json({ message: "No token, authorization denied" });
-  jwt.verify(token, JWT_SECRET!, async (error, decoded) => {
-    if (error) res.status(401).json({ message: "Token is not valid" });
-    const user = decoded as UserId;
-    const userFound = await UserModel.findById(
-      user?.id as mongoose.Types.ObjectId
-    );
-    if (!userFound) res.status(401).json({ message: "Token is not valid" });
-    res.status(200).json({
-      id: userFound?._id,
-      username: userFound?.username,
-      email: userFound?.email,
-      updatedAt: userFound?.updatedAt,
-      createdAt: userFound?.createdAt,
-    });
-  });
+  jwt.verify(
+    token,
+    JWT_SECRET!,
+    async (error: jwt.VerifyErrors | null, decoded: any) => {
+      if (error) res.status(401).json({ message: "Token is not valid" });
+      const user = decoded as UserId;
+      const userFound = await UserModel.findById(
+        user?.id as mongoose.Types.ObjectId
+      );
+      if (!userFound) res.status(401).json({ message: "Token is not valid" });
+      res.status(200).json({
+        id: userFound?._id,
+        username: userFound?.username,
+        email: userFound?.email,
+        updatedAt: userFound?.updatedAt,
+        createdAt: userFound?.createdAt,
+      });
+    }
+  );
 };
