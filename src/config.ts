14829758a1eb353f17d56c0a76790ee477535ead
Sallanez <saul.alejandro19@gmail.com>
2023-09-26 14:56:13 -0600

    process.env ?

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,16 @@ import dotenv from "dotenv";
 
 const envFound = dotenv.config();
 
-export const PORT = envFound.parsed?.API_PORT;
-export const JWT_SECRET = envFound.parsed?.API_JWT_SECRET;
-export const MONGO_USER = envFound.parsed?.API_MONGO_USER;
-export const MONGO_PASSWORD = envFound.parsed?.API_MONGO_PASSWORD;
-export const ORIGIN_DOMAIN = `${envFound.parsed?.API_ORIGIN_DOMAIN}:${envFound.parsed?.API_ORIGIN_PORT}`;
+export const PORT = envFound.parsed?.API_PORT || process.env.API_PORT;
+export const JWT_SECRET =
+  envFound.parsed?.API_JWT_SECRET || process.env.API_JWT_SECRET;
+export const MONGO_USER =
+  envFound.parsed?.API_MONGO_USER || process.env.API_MONGO_USER;
+export const MONGO_PASSWORD =
+  envFound.parsed?.API_MONGO_PASSWORD || process.env.API_MONGO_PASSWORD;
+export const ORIGIN_DOMAIN =
+  `${envFound.parsed?.API_ORIGIN_DOMAIN}:${envFound.parsed?.API_ORIGIN_PORT}` ||
+  `${process.env.API_ORIGIN_DOMAIN}:${process.env.API_ORIGIN_PORT}`;
 
 //For the script set-env.ts
 export const NODE_API_PORT = envFound.parsed?.API_PORT;
