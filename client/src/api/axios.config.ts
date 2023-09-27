7ed7c8f8310a24d2ae4b752b9378c3f62229a677
Sallanez <saul.alejandro19@gmail.com>
2023-09-27 10:39:13 -0600

    empty string test

--- client/src/api/axios.config.ts
+++ client/src/api/axios.config.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const uri = import.meta.env.VITE_NODE_API_PORT
   ? `${import.meta.env.VITE_NODE_API}:${import.meta.env.VITE_NODE_API_PORT}`
-  : `${import.meta.env.VITE_NODE_API}`;
+  : '';
 console.log(uri);
 const instance = axios.create({
   baseURL: uri,
