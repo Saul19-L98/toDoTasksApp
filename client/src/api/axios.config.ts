8c3497c34a15814334da79122d22951d72d97648
Sallanez <saul.alejandro19@gmail.com>
2023-09-27 10:28:27 -0600

    axios.config.ts updated, uri validation added

--- client/src/api/axios.config.ts
+++ client/src/api/axios.config.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const uri =
-  `${import.meta.env.VITE_NODE_API}` +
-  (process.env.VITE_NODE_API_PORT && `:${import.meta.env.VITE_NODE_API_PORT}`);
-
+const uri = import.meta.env.VITE_NODE_API_PORT
+  ? `${import.meta.env.VITE_NODE_API}:${import.meta.env.VITE_NODE_API_PORT}`
+  : `${import.meta.env.VITE_NODE_API}`;
 console.log(uri);
 const instance = axios.create({
   baseURL: uri,
